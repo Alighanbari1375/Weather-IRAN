154287731bce7268fc88a82334ab029f4cf9df21
Alighanbari1375 <alighanbari137513751375@gmail.com>
2025-01-04 00:25:15 +0330

    Display the result of the weather

--- input-weather.js
+++ input-weather.js
@@ -1,7 +1,8 @@
 document.getElementById('resullte').addEventListener('click', function () {
     const city = document.getElementById('input-text').value;
-    const apiKey = "10e19306ca0c4757a9d194225250301";
+    const apiKey = "10e19306ca0c4757a9d194225250301"; 
     const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+    
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -10,13 +11,15 @@ document.getElementById('resullte').addEventListener('click', function () {
             return response.json();
         })
         .then(data => {
-            console.log(JSON.stringify(data));
-            // const temp = data.main.temp;
-            // const cityName = data.name + ', ' + data.sys.country;
-            // const weatherDescription = data.weather[0].description;
-            // document.getElementById('num1').innerText = `${temp}°`;
-            // document.getElementById('cityName').innerText = cityName;
-            // document.getElementById('weatherDescription').innerText = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+            console.log(JSON.stringify(data)); 
+
+            const temp = data.current.temp_c;  
+            const cityName = data.location.name + ', ' + data.location.country; 
+            const weatherDescription = data.current.condition.text; 
+            
+            document.getElementById('num1').innerText = `${temp}°`;
+            document.getElementById('cityName').innerText = cityName;
+            document.getElementById('weatherDescription').innerText = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1); // نمایش 
         })
         .catch(error => {
             document.getElementById('num1').innerText = 'خطا: ' + error.message;
